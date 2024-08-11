1a54bb80612335e860c931d9430b70e44a2a652f
goodrussian666 <162756409+goodrussian666@users.noreply.github.com>
2024-08-11 20:33:51 +0200

    Update link_libsodium.sh

--- link_libsodium.sh
+++ link_libsodium.sh
@@ -10,12 +10,8 @@ test -d tmp/libsodium || mkdir -p tmp/libsodium
 
 cd tmp/libsodium
 
-echo "point 1......"
-
 curl --retry 5 --retry-delay 0 -sL https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VERSION-RELEASE/libsodium-$LIBSODIUM_VERSION.tar.gz -o libsodium-$LIBSODIUM_VERSION.tar.gz
 tar xfz libsodium-$LIBSODIUM_VERSION.tar.gz --strip-components=1
-
-echo "point 2......"
 
 CONFIGURE_ARGS="--prefix ${PWD}"
 if [[ "${OS}" == "SunOS" ]]; then
@@ -23,15 +19,21 @@ if [[ "${OS}" == "SunOS" ]]; then
   CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-ssp"
 fi      
 
-echo "point 3......"
-
 ./configure ${CONFIGURE_ARGS}
 
 echo "point 4......"
 
-make && make check && make install
+make 
 
 echo "point 5......"
+
+make check 
+
+echo "point 6......"
+
+make install
+
+echo "point 7......"
 
 # Remove shared libraries for using static
 rm -f lib/*.so lib/*.so.* lib/*.dylib
